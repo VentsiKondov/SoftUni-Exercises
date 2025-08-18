5388576d3fb371b912c31f6850d92227db752855
VentsiKondov <ventsibg1112@gmail.com>
2025-08-18 20:32:06 +0300

    added another solution

---
+++ Nested Loops - Exercise/5_special_numbers.py
@@ -0,0 +1,17 @@
+number = int(input())
+for number_one in range(1, 10):
+    if number % number_one != 0:
+        continue
+
+    for number_two in range(1, 10):
+        if number % number_two != 0:
+            continue
+
+        for number_three in range(1, 10):
+            if number % number_three != 0:
+                continue
+
+            for number_four in range(1, 10):
+                if number % number_four != 0:
+                    continue
+                print(f"{number_one}{number_two}{number_three}{number_four}", end=" ")
