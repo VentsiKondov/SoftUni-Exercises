df20410f221124a3cdb4086dff461f44be0c6d5d
VentsiKondov <ventsibg1112@gmail.com>
2025-09-29 00:26:53 +0300

    added another solution

---
+++ Python Fundamentals/Functions - Exercise/7_min_max_sum.py
@@ -0,0 +1,6 @@
+random_numbers = list(map(int, input().split()))
+
+min_number =  min(random_numbers)
+max_number = max(random_numbers)
+sum_of_numbers = sum(random_numbers)
+print(f'The minimum number is {min_number} \nThe maximum number is {max_number} \nThe sum of numbers is {sum_of_numbers}')
