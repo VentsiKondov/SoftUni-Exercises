e7182b587cbae4cf4ed302ff14cbef92c694654d
VentsiKondov <ventsibg1112@gmail.com>
2026-01-16 15:02:26 +0200

    added another solution

---
+++ Python Advanced/Lists as Stacks and Queues - Lab/5_hot_potato.py
@@ -0,0 +1,13 @@
+from collections import \
+    deque
+
+kids = deque(input().split())
+toss =  int(input())
+
+while len(kids) != 1:
+    kids.rotate(-toss+1)
+    print(f'Removed {kids.popleft()}')
+print(f'Last is {kids.pop()}')
+
+
+
