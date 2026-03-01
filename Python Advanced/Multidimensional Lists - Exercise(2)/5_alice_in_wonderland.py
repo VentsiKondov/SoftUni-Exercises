288b81ca6f87c13d6e286a01ef6dbff7d678b8b3
VentsiKondov <221202398+VentsiKondov@users.noreply.github.com>
2026-03-01 19:59:39 +0200

    added solution

---
+++ Python Advanced/Multidimensional Lists - Exercise(2)/5_alice_in_wonderland.py
@@ -0,0 +1,50 @@
+size = int(input())
+directions = {"up": [-1,0], "down": [1,0], "left": [0,-1], "right": [0,1]}
+alice_position = []
+have_the_tea = False
+matrix = []
+tea_bags = 0
+for row in range(size):
+    matrix.append(input().split())
+
+    if "A" in matrix[row]:
+        alice_position = [row, matrix[row].index("A")]
+        matrix[alice_position[0]][alice_position[1]] = '*'
+
+
+while tea_bags < 10:
+    direction = input()
+
+    row = alice_position[0] + directions[direction][0]
+    col = alice_position[1] + directions[direction][1]
+
+    if not (0 <= row < size and 0 <= col < size):
+        break
+
+    alice_position = [row, col]
+    position = matrix[row][col]
+    matrix[row][col] = '*'
+
+    
+    if position == "R":
+        break
+
+
+    if position.isdigit():
+        tea_bags += int(position)
+
+
+
+
+if tea_bags >= 10:
+    print(f'She did it! She went to the party.')
+else:
+    print(f"Alice didn't make it to the tea party.")
+
+[print(*row) for row in matrix]
+
+
+
+
+
+
