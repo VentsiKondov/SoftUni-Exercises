8554402ccfca7ec1f690e10566d7b54214a557ce
VentsiKondov <221202398+VentsiKondov@users.noreply.github.com>
2026-08-09 23:46:15 +0300

    added solution

---
+++ Python Algorithms/recursion/3_move_down_right.py
@@ -0,0 +1,14 @@
+def moving(row,col,rows,cols):
+    if row >= rows or col >= cols:
+        return 0
+    if row == rows -1 and col == cols - 1:
+        return 1
+
+    result = 0
+    result += moving(row+1,col,rows,cols)
+    result += moving(row,col+1,rows,cols)
+    return result
+
+rows = int(input())
+cols = int(input())
+print(moving(0,0,rows,cols))
